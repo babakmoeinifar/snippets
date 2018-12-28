70647b82f969dc6eeaedcef2223a1d34a2174d2e
babakdev <babakus7@gmail.com>
2018-12-28 08:37:51 +0330

    LPIC1 done

--- snippets.php
+++ snippets.php
@@ -9,6 +9,12 @@ php artisan migrate:rollback --step=3
 <small>{{$errors->first('fieldName')}}</small>
 
 <li class="{{Request::is('/') ? active : ''}}"><a href="/">Home</a></li>
+
+//drop a table from tinker
+php artisan tinker
+//Then
+Schema::drop('books')
+
 //=====================================validate===========================================
 $this->validate($request,[
 'image'		=>'mimes:jpg,jpeg,bmp,png | max:500',
@@ -21,8 +27,6 @@ $this->validate($request,[
 'military'	=>['required_if:gender,male , in:payanKhedmat,moaf'],
 'password'  => 'required_without:user_id',
 'field_c'   => 'required_with:field_d',
-
-
 ]);
 
 //=====================================response===========================================
@@ -337,24 +341,6 @@ laravel ecommerce lesson #77 in minute 15 has a good example for using functions
 if($('#paypal').is(':checked')){ alert('do something');}
 
 
-
-
-
-
-//=====================================mysql=========================================
-
-//give access to a db from a user
-GRANT ALL on dbName TO 'userName'@'localhost';
-
-//Lynda tut 018 Use environment variables
-//first remove db name line compelete from .env file
-//ست کردن دیتابیس پروژه از مای اسکیول که این دیتابیس ساخته شده و با این دستور تخصیص میده
-set -x DB_DATABASE db_irenkala
-
-//drop a table from tinker
-php artisan tinker
-//Then
-Schema::drop('books')
 //=====================================phpstorm=========================================
 alt+up or alt+down 	move to methods
 alt+j 		 select same words
@@ -367,29 +353,55 @@ git tag
 git push origin --tags
 git checkout v0.2
 
+//=====================================sql==========================================
+SHOW DATABASES;
+USE databaseName;
+SHOW TABLES;
+SELECT * FROM tableName ORDER BY name DESC;
+SELECT name, email FROM tableName WHERE name = 'babak' LIMIT 3;
+SELECT * FROM tableName WHERE name LIKE 'ba%' AND weight > 80 GROUP BY weight;
+SELECT count(mood), mood FROM info GROUP BY mood;
+INSERT INTO phonebook (name, email) VALUES ('babak', 'babakus7@gmail.com');
+INSERT INTO phonebook VALUES ('babak', '0918', 'babakus7@gmail.com');
+DELETE FROM phonebook WHERE name = 'babak';
+UPDATE phonebook SET email = 'info@fff.com' WHERE name = 'babak';
+SELECT * FROM phonebook JOIN info ON phonebook.name = info.name;
+SELECT phonebook.name, phone FROM phonebook JOIN info ON phonebook.name = info.name WHERE 'mood' = 'sad';
+
+//give access to a db from a user
+GRANT ALL on dbName TO 'userName'@'localhost';
+
+//Lynda tut 018 Use environment variables
+//first remove db name line compelete from .env file
+//ست کردن دیتابیس پروژه از مای اسکیول که این دیتابیس ساخته شده و با این دستور تخصیص میده
+set -x DB_DATABASE db_irenkala
 
 //=====================================linux=========================================
+middle click after select command in bash will copy paste into new command
 ctrl+f1		go to console when gdm not working
 ctrl+r      search in terminal
 sudo systemctl disable docker.socket
 dmesg 		show log from init system
 lsmod 	rmmod 	modprob 	- lspci 	lsusb 	lshw 	lshal 
-ls | less 		ls | grep felan
+ls | less 		ls | grep felan   		ls name*
 pstree | less 		-		pstree | head 		-		pstree | tail   (bottom)
 tail /var/log/syslog 		(آخرین اتفاقاتی که تو سیستم افتاده)
 //for system monitoring on any file or log file
 tail -f /var/log/syslog 		(آخرین اتفاقاتی که تو سیستم افتاده و باز میمونه و اتفاقات جدید مثل پلاگ شدن رو نشون میده)
 whereis		-		ldd felanApp (see libraries that using)
-jcal 					taghvim
+file fileName.txt   /*shows type of file*/
+jcal 					/*taghvim*/
+links2 weecode.ir/      /*links app*/
 man commandName 		(press h in manual)
 tree dirName
-
+eject 					/*ejects cdrom*/
 ln fromFile.txt toFile.txt 			(hardlink-same inode)
 unlink hardLinkFile.txt 			(removes linked file)
 ln -s newFile.txt softlink.txt 		(**softlink-different inode) (softlink.txt -> newFile.txt)
 which subl 	- 	ls -ltrh outputOfWhich 		
 
 //------------------------bash-----------------------------
+shutdown -h now
 echo -e "hello\n"		
 ls; echo done; 			(two seperate command in one line) 
 ls && echo done 		(two command in one line that must execute all if one not run others wont run) echo $?
@@ -408,17 +420,24 @@ PATH=$PATH:/home/babak/bin 		(add environment path)
 vi .bashrc export PATH="$PATH:/home/babak/bin/"                     (add permanet environment path)
 echo $PATH - export PATH=copy from echo $PATH and change it 		(edit environment path)
 
+/etc/profile 	shell configuration
+alias 			(shows all aliases)
+funcName(){/*commands*/} 	- call-> funcName()
+env 		- env babak=happy 		- env --unset babak
+/**************/	php /var/www/html/artisan queue:work	/**************/
+
 //------------------------streams----------------------------
 ls -1 | sort 	----	ls -1 | sort -r   ----	sort file
 sort -n file    					(sort in numeric)
-sort file | uniq -c{for count} -u{doesnt show doubled word} -d{just shows doubled word}	 	(remove same words and shows just one of theme) 	{sort is nessessary in this command}
-ls > newFile 	(خروجی را بریز به فابل) (redirect datas to newFile)
+sort file | uniq -c /*{for count}*/ -u /*{doesnt show doubled word}*/ -d /*{just shows doubled word}*/
+/*(remove same words and shows just one of theme)*/ 	/*{sort is nessessary in this command}*/
+ls >> newFile 	(خروجی را بریز به فابل) (redirect datas to newFile)(>> is add and append)
 ls x* m* > outputFile 2> errorFile or &> &>> (خروجی استاندارد را بریز به outputFile و خروجی ارور را بریز به errorFile)
 ls a* >> outputFile 			(محتویات فایل را حذف نکن فقط خروجی جدید را تهش اضافه کن)
 ls a* x* 1> outputFile 2>&1 	خروجی ارور را بریز همونجا که خروجی استاندارد رو میریزی
 ls a* x* > /dev/null 2> errorFile 	خروجی استاندارد را بریز تو هیچجا و نمایش نده
 cat -n myFile        or         nl myFile 		(شماره گذاری خطوط خروجی)
-cut -f1 d, 		(cut filed 1 seperator is ,)
+cut -f1 d, 										(cut filed 1 seperator is ,)
 paste 		join
 cat file | tr ' ' '-' > newFile 	(translate from ' ' to '-') 	- {tr cant run alone}
 cat file | tr '\t' ' ' | tr ' ' '-'
@@ -432,7 +451,7 @@ sed 's/you/shoma' file
 {regex can use in sed command like} sed -r "s/^a/b/"
 
 //------------------------file management-----------------------
-ls -1 	{shows list 1 line 1 line} 		-	ls -R (Recursive) 	- rm -R myDir 	-  ls *jpg 
+ls -1 -a 	{shows list 1 line 1 line - (a) is hidden} 		-	ls -R (Recursive) 	- rm -R myDir 	-  ls *jpg 
 ls -ltrhi 	{shows list long and (h)human readable and (t)sort in time created and (r)reverse time and (i) inode}
 cp source destination 		- 	mv file1 file2 file3 dir 	(multiple move-last one is destination)
 mv myFile newFile 		(rename directory)
@@ -484,22 +503,22 @@ ps -C xeyes --sort +comm,-sid -o user,pid,time,comm 	(C is search for)(o is Outp
 free -m or -g (shows free ram in mb or gb)
 
 //-------------------------------vi----------------------------------
-i  insert mode 		-press Esc then -> u undo in command mode
-replace a word in command mode on a word -> c then w
-delete a word in command mode on a word  -> d then w
-/ search forward 	- ? search backward
+i  /*insert mode*/ 		-press Esc /*then*/ -> u /*undo in command mode*/
+/*replace a word in command mode on a word*/ -> c /*then*/ w
+/*delete a word in command mode on a word*/  -> d /*then*/ w
+/ /*search forward*/ 	- ? /*search backward*/
 :q 	-:q!  !یعنی مطمعنم دارم چکار میکنم 	-:w! 
 :w (write file) 	-:w myFile.txt (write to new file) 
-:e! if you fucked file it reloads file from disk and refresh it
+:e!    :qa! /*if you fucked file it reloads file from disk and refresh it*/
 :! (runs shell commands) --like--> :!ls
 ZZ (Exit and save if  modified- without : - its faster than :wq if large file not modified)
 :help copy    -  :help save 
 
-x cut
-dd Delete Line
-p paste last deleted text after cursor	-	P before
+x  /*cut*/
+dd /*Delete Line*/
+p  /*paste last deleted text after cursor*/	-	P /*before*/
 
-//-----------------------------Partiotions----------------------------
+//-----------------------------Partitions----------------------------
 sudo fdisk -l /dev/sda
 sudo fdisk  /dev/sda
 sudo gdisk /dev/sda 	(modern command) (GPT disks) (in gdisk you can change partition name)
@@ -508,7 +527,11 @@ sudo mkfs -t ext4 -L driveName /dev/sda2 	(make file system == format) 	- sudo m
 ls /sbin/mk* 	(shows all mkfs commands)
 mkswap /dev/sda3
 blkid /dev/sda1		(returns UUID of disk)
-
+{
+fallocate -1 1G /swapfile
+mkswap /swapfile
+swapon /swapfile
+}
 mount /dev/sda1 /media/babak	- mount -o ro /dev/sda1 /media/babak 	(o is output - ro is readonly - rw)
 umount /media/babak   or    umount /dev/sda1		- mount  (shows all mounted disks)
 swapon -a 		- 	swapoff   - swapon -s
@@ -521,4 +544,168 @@ sudo tune2fs -l /dev/sda1
 df -TH 				(disk free remains) (H or h human) (T for disk type)
 du /Desktop -hc 	(disk usage) (c for total) (-s for just summary)
 sudo debugfs /dev/sdb1
-//---------------------------------------------------------------------
+
+//----------------------------------scripts----------------------------------
+BABAK=$(ls -1) 		- echo $BABAK 		 /*this is good for chmod files in loop on server*/
+today=`date +%Y%m%d-%H%M%S` 	- echo 'some text' > $today.log
+vim hi.sh 			<-write
+sh hi.sh    		<-run
+chmod +x hi.sh  	<-executable 		- or chmod 755
+./hi.sh    			<-run in executable mode
+//---in writing script---  
+	#!/bin/bash
+	echo  /*new line*/ 
+	read NAME
+
+	if [ $NAME = 'babak' ]
+	then
+		//do
+	else
+		//do
+	fi
+	
+	for NUM in 1 2 3; 		//or   for FILE in $(ls) 		//or 	for NUM in `seq 1 50`   //or   in $(ls -1)
+	do 						//	   do
+		echo $NUM 			// 			wc $FILE 			// or   chmod -R 755 $FILE
+	done 					//	   done
+
+	$VAR=25
+	while [ $VAR -gt 42]
+	do
+		echo $VAR
+		let VAR=VAR-1
+	done
+
+//in script writing
+echo "body of message or $VAR" | mail -s "subject" root
+
+//------------------------------------users-groups-------------------------------
+useradd babak -aG www-data     /*(aG is add to additional Group)*/ 		- adduser babak
+passwd 						   - /*in sudo mode*/ -> passwd babak
+/etc/skel/ 		هر کاربر جدید که ساخته بشه محتویات این فولدر را براش کپی میکنه
+usermod -aG -L -U 	/*(L->Lock this account)(U->Unlock)(aG->add To More Groups)-mod is modify*/
+usermod -c 'mr babak' babak 	/*(c->comment name for display)*/
+usermod -s /bin/zsh babak   	/*(s->specify shell)*/
+userdel -r babak 				/*(r->remove /home/babak/)*/
+groupadd newGroup
+groupdel newGroup
+
+/etc/passwd
+chage babak				 - chage -l babak 			/*list information about expire password*/
+id babak 											/*shows id of user and groups*/
+getent passwd babak      - getent hosts localhost   /*get entry of ...*/
+usermod -s /sbin/nologin gdm  						/*it prevents login for gdm user*/
+
+//----------------------------Automate System Administration---------------------------
+crontab -l 		/*shows corns*/ /*it's location -> /var/spool/cron/crontabs/USERNAME*/
+crontab -e      /*e for edit*/
+1 * * * * /home/babak/bin/script.sh /* m=minute, h=hour, dom=day of month, mon=month, dow=day of week*/
+/*for example  -->  14 15 1 * * --> 15:14 monthly*/ ( */5 * * * * ) /*is every 5 minutes*/
+0 8 * * 3-6/2 /*یک روز در میان */ 		1 */8 * * * /*do it per 8 hours*/ 42 2 1 * * /*for backup monthly*/
+@reboot  - @daily 	/*instead of m h dom mon dow*/
+
+at now + 2 min 		- at 8:00 -> shutdown -h now    - at tomorrow   /*next 24h*/
+atq 				/*shows queue of ats*/     atrm {number of atq}  /*remove at*/
+
+/etc/cron.d/ 		/*system wide cron*/
+/etc/anacrontab 	// if system was off in that hour that should runs a cron it is better to use anacrontab
+
+/etc/cron.allow   -> babak 		or 		/etc/cron.deny           /*just one of them could create*/
+/etc/at.allow     -> babak 		or 		/etc/at.deny			 /*just one of them could create*/
+
+//-----------------------------------Localization-------------------------------------
+locale 		/*language*/		- dpkg-reconfigure locales
+export LC_ALL='fa_IR.UTF8' 		- unset LC_ALL 		- export LANG=C   /*is for programming*/
+iconv -f WINDOWS-1258 -t UTF-8 file.txt 		/*converts -f from -t to */
+
+tzselect		/*changes local timezone after that command-> */  export TZ='Asia/Tehran'
+/*1. edit system wide time zone*/ 				/etc/timezone	
+/*2.in /etc/localtime*/		unlink localtime    
+/*3.*/ ln -s /usr/share/zoneinfo/Asia/Tehran localtime
+
+ntpdate pool.ntp.org 							/*gains correct time from that server*/
+hwclock -u -w  									/*sets the hardware clock from systemtime*/ 
+apt install ntp 	- systemctl enable ntp 		/*sets time automatic always instead of ntpdate */
+ntpq -p 										/*prints what is doing*/
+
+//-------------------------------------system logging-----------------------------------
+journalctl 							/*it's config is in /etc/systemd/journal.conf*/ /*var/log/journal/*/
+logger local1.info babak was here   /*logger (facility) (user or @ip or mail) (action)*/
+/*facility.info or emerg/panic or alert or crit or err/error or warn/warning or notice or info or debug */
+logrotate 							/*app for removing loggs*/
+ls -ltrh /var/log   /*to see what file has benn written lately then */ tail -f daemon.log
+
+//---------------------------------Mail Transfer Agent MTA-------------------------------
+cat /etc/aliases 		 /*shows all mail aliases for froward mail from: to*/ /*after edit*/ -> newaliases
+vim ~/.forward           /*in '~/' directory and put */ babakus7@gmail.com /*forwards locally mails without sudo*/
+mailutils 	-> mail babak    - mail babakus7@gmail.com
+mail 		- 	mail root	
+d -> /*delete*/  										   d 1-5 -> /*delete 1 to 5 mails*/ 
+echo -e remember to bring felan | mail -s "todo" babakus7@gmail.com /*useful for note for myself*/
+mailq      /*queue of mails*/
+
+//------------------------------------Manage Printers-----------------------------------
+sudo systemctl start cups
+localhost:631					   /*cups port in browser*/
+lpr file.txt /*print file*/	- lpq  /*prints queue*/		- lprm 2 /*remove file from queue*/	
+lpr -Pxerox_printer file.txt       /*choose printer*/   - lpq -Pxerox_printer 		- lpq -a      /*all*/
+lprm -  /*removes all print jobs*/		- sudo lpc status /*printer controll- list of printers*/
+cupsenable my_xerox - cupsdisable  -> /*for print phisically*/ 
+cupsaccept 	- cupsreject my_xerox -r 'chon por shode' -> /*for accept queue  -r is reason*/
+
+//-------------------------------------Network-------------------------------------------
+ifconfig   /*shows enabled network devices*/ 		ifconfig -a  /*shows all network devices*/
+ifoconfig enp0s25               /*shows specific NIC*/
+ifoconfig enp0s25 192.168.1.2 	/*set ip temporary for specific NIC enp0s25 is NIC name comes from ifconfig command*/
+/etc/network/interfaces  -> address 192.168.1.2       	  /*sets ip permanently*/
+ifoconfig enp0s25 192.168.1.2 netmask 255.255.255.0       /*set ip with netmask*/ 
+/etc/network/interfaces  ->   netmask 255.255.255.0       /*sets ip permanently*/
+/etc/network/interfaces  ->   gateway 192.168.1.1         /*sets gateway permanently*/
+ifoconfig enp0s25 down
+ifoconfig enp0s25 up
+/etc/network/interfaces.d/         /*config file for set ip permanently*//*create file eth0*/
+/etc/resolve.conf 									  /*set DNS temporary*/
+/etc/network/interfaces  -> dns-nameservers 4.2.2.4   /*set DNS permanently*/
+/etc/hostname						/etc/hosts             getent hosts    /*shows all hosts*/     getent dbName key    - getent hosts iren.me
+route      or   netstat -nr 				/*shows route table*/
+route add default gw 192.168.1.1            /*set default gateway we can see gateway from route command*/
+
+ip addr show    									/*it is newer command than ifconfig*/
+ping -n yahoo.com 									/*shows ip of domain*/
+netstat -na | head  یا  grep ":80" 					/*shows which ports are listening and established -a is active*/
+traceroute google.com 				->				tracepath google.com
+dig google.com 										netcat  or  nc 				/*cat command for network*/
+
+//---------------------------------Security Administration-------------------------------
+find / -perm -u+s /*find files that their permision is SUID for apps*/
+/*باراول که سیستمی تحویل میگیریم خروجی این دستور رو تو فایل ذخیره میکنیم برای مقایسه بعدا*/
+netstat -tuna	-	netstat -pa | grep LISTEN | less	 		/*see open ports*/
+fuser 80/tcp 	-	lsof -i 									/*see open ports*/ /*-i for internet connections*/
+nmap localhost or google.com 									/*analyze open ports */
+su jafar -	/*login to jaffar with jafar password*/ 			/*su root login to root with root password*/
+sudo su -   /*login to root with my password*/					visudo 	- sudo usermod -aG sudo babak		/*add user to sudoers*/
+ulimit -a   /*shows limit of anything*/ 	/*export of command shows -t or -harchi for reaching that*/
+ulimit -t 1 					/*sets cpu time to 1 for this user temporary - for resource management*/
+/etc/security/limits.conf 		/*sets limit permanently*/
+w 	-	who	   					/*checking users in the system*/
+last 							/*shows logins of user*/ 		last -f /var/log/btmp 		/*shows failed logins*/
+
+//====security host====
+/etc/nologin 			/*create if you need and in that for example -> system is down. come back 11PM*/
+/etc/services 			/*shows all defined ports in system*/
+/etc/hosts.allow 			/etc/hosts.deny
+
+//===encryption===
+ssh-keygen -t rsa -b 4096 -C "BABAK"    /*copy this ssh and in server paste it in ~/.ssh/authorized_keys for strait connect to server*/
+or ssh-copy-id weecode.ir
+ssh -T git@gitlab.com
+ssh-agent 					/*Management ssh keys*/ 		ssh-add -D  	/*deletes ssh keys*/ ssh-add /*add theme again*/
+ssh-add -l 					/*add ssh to ssh-agent*/		/*l for list of finger prints*/
+/etc/ssh       				/*for all users*/
+gpg --gen-key        		gpg --export babak > babak_public_key.pub 	/*to share our public key to other people then send it to him/her */
+/*in second system*/		gpg --import babak_public_key.pub 			/*write a letter for babak -> letter.txt*/ 
+/*encrypt file*/			gpg --out letter_gpg.txt --recipient babak --encrypt letter.txt 	/*send letter_gpg.txt to babak*/
+/*open encrypt file*/ 		gpg --out letter_decrypted.txt --decrypt letter_gpg.txt 
+gpg --output revoke.asc --gen-revoke babak  		/*recover private gpg key if it lost*/
+gpg -clearsign fileName 							/*sign a file*/
+gpg --verify fileName 								/*check in other system that signature is OK or not*/ 

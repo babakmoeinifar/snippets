6939b232ddae608b2bd558f8654eb92705409121
Babak Moeinifar <babakus7@gmail.com>
2024-12-01 12:24:57 +0330

    Update 2024.php

--- 2024.php
+++ 2024.php
@@ -6,3 +6,24 @@ $contacts->where('params->vendor_profile', '!=', null);
 
 -------------
 img{ aspect-ratio:16/9 or 4/4 }
+-------------
+ public static function firstIdOfMyPeopleTeam()
+    {
+        $myTeamId = auth()->user()->struct->team_id ?? null;
+
+        // Use a recursive CTE to find the top-level team
+        return PeopleTeam::withRecursiveExpression(
+            'team_hierarchy',
+            PeopleTeam::where('id', $myTeamId)
+                ->union(
+                    PeopleTeam::whereColumn('id', 'team_hierarchy.parent_id')
+                ),
+            ['id', 'parent_id'], // Specify columns as an array
+            null,
+            'parent_id'
+        )
+            ->where('parent_id', 0)
+            ->first();
+    }
+//مثال: اگر شما در یک سازمان هستید و می‌خواهید بفهمید بالاترین تیم بالادستی شما کدام است، از این کد استفاده می‌کنید.
+-----------
